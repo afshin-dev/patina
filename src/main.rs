fcb607dd3fd6bef7b393cd0229b5cdef87073609
afshin <shahinof10009@gmail.com>
2023-11-09 18:52:17 +0330

    simple wait function with closure and requlare function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::{thread, time};
+
 fn main() {
-    println!("Hello, world!");
+    // closure example of `wait_a_sec`
+    let wait_a_second = |n: u64| -> () {
+        for i in 0..n {
+            thread::sleep(time::Duration::from_secs(1));
+            println!("second {i}")
+        }
+    };
+    wait_a_sec(3);
+    wait_a_second(3);
+}
+
+fn wait_a_sec(second: u64) -> () {
+    for i in 0..second {
+        thread::sleep(time::Duration::from_secs(1));
+        println!("sec {i}");
+    }
 }
